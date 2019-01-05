a3ffe0d0d083a9bc87db5db99214b295d9cb4dfa
Nabil Gishkori <nabilgishkori@gmail.com>
2019-01-05 19:38:17 +0000

    Initial commit

---
+++ Topic.py
@@ -0,0 +1,36 @@
+import pickle
+
+class Topic:
+
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        self.avgScore = 0
+        self.tested = False
+        self.questions = []
+        self.scores = {}
+        self.allQuestionsEntered = False
+
+    def addQuestions(self, rewrite = False):
+        end = False
+        if rewrite:
+            self.questions = []
+        while end == False:
+            question = input(self.category + ', ' + self.name + ": Enter the next question: ")
+            if len(question) > 0:
+                if question == 'delete':
+                    self.questions = self.questions[:-1]
+                elif question == 'restart':
+                    self.questions = []
+                else:
+                    self.questions.append(question)
+            else:
+                end = True
+                self.allQuestionsEntered = True
+
+        self.save()
+
+    def save(self):
+        with open('Topics/TopicObjects/' + self.category + '/' + self.name + '.pickle', 'wb') as file:
+            pickle.dump(self, file)
+
---
+++ topicPopulation.py
@@ -0,0 +1,53 @@
+import os
+import pickle
+from Topic import *
+
+categories = ['physiology', 'physics', 'pharmacology', 'clinical']
+
+def getTopicList(category):
+    topicList = []
+    with open('Topics/' + category + 'Topics.txt', 'r') as file:
+        content = file.readlines()
+    for line in content:
+        fullStop = line.index('.')
+        topic = line.replace(".", "").strip()
+        topicList.append(topic)
+
+    return topicList
+
+def createTopics(category):
+    topicList = getTopicList(category)
+    for topic in topicList:
+        newTopic = Topic(topic, category)
+        newTopic.save()
+
+def populateQuestions():
+    for category in categories:
+        path = 'Topics/TopicObjects/' + category
+        for pickleFile in os.listdir(path):
+            with open(path + '/' + pickleFile, 'rb') as file:
+                topicObj = pickle.load(file)
+            if not topicObj.allQuestionsEntered:
+                topicObj.addQuestions()
+
+
+
+populateQuestions()
+# for category in categories:
+#     path = 'Topics/TopicObjects/' + category
+#     files = os.listdir(path)
+#     for file in files:
+#         with open(path + '/' + file, 'rb') as pickleFile:
+#             topicObj = pickle.load(pickleFile)
+#         topicObj.name = file.replace(".pickle", "")
+#         topicObj.save()
+
+# path = 'Topics/TopicObjects/physics'
+# file = "55 General aspects of pressure.pickle"
+# with open(path + '/' + file, 'rb') as pickleFile:
+#             topicObj = pickle.load(pickleFile)
+# print(topicObj.questions)
+#
+# topicObj.allQuestionsEntered = False
+# topicObj.addQuestions()
+# topicObj.save()
